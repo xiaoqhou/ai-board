904ff10325a125af1efab653ccc3f0933dcfc15b
Steven Hou <houxq.bj@outlook.com>
2026-05-09 19:33:42 +0000

    change to dockerfile

---
+++ .devcontainer/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Codespace nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    basePackages = with pkgs; [
+      #git-lfs
+      direnv
+      devbox
+      home-manager
+      opencode
+    ];
+  in
+  {
+    # run 'nix profile add .' to install base packages
+    packages.${system}.default = pkgs.buildEnv {
+      name = "codespace-base";
+      paths = basePackages;
+    };
+
+  };
+}
